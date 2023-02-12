23d29593bbc670cbc925853493750f1e760867b8
Parutin Maxim <parutin.ma@phystech.edu>
2023-02-13 01:46:42 +0300

    test commit

---
+++ getting_info/get_info.py
@@ -0,0 +1,50 @@
+import vk_api
+from login_credits import phone_number, password
+from pprint import pprint
+
+
+vk_session = vk_api.VkApi(phone_number, password)
+vk_session.auth()
+
+vk = vk_session.get_api()
+
+GROUP_ID = -188660528
+
+def getData(vk_api, group_id, voting_flg=True):
+    users_and_answers = []
+    items = vk_api.wall.get(owner_id=group_id)["items"]
+    for item in items:
+        attachments = item["attachments"]
+        for attachment in attachments:
+            if attachment["type"] == "poll":
+                poll = attachment["poll"]
+                answers = poll["answers"]
+                answer_ids = list(map(lambda x: x["id"], answers))
+                poll_id = poll["id"]
+                tmp_query = "SELECT * FROM tablename WHERE poll_id="
+                
+                
+                
+                if voting_flg:
+                    vk_api.polls.addVote(
+                        owner_id=group_id, 
+                        poll_id=poll_id,
+                        answer_ids=answer_ids[:1])
+                           
+                voters = vk.polls.getVoters(
+                    owner_id=group_id,
+                    poll_id=poll_id,
+                    answer_ids=answer_ids)
+                
+                for voter in voters:
+                    answer_id = voter["answer_id"]
+                    for user_id in voter["users"]["items"]:
+                        user_and_answer = {
+                            "user_id"  : user_id,
+                            "answer_id": answer_id,
+                            "poll_id"  : poll_id
+                        }
+                        users_and_answers.append(user_and_answer)
+    return users_and_answers
+
+pprint(getData(vk, GROUP_ID, voting_flg=False))
