3d78405825f9b09ec4ce523b83705b74033ba88c
molozey <lipskii.as@phystech.edu>
2023-02-17 15:39:57 +0300

    captcha executor

--- getting_info/get_info.py
+++ getting_info/get_info.py
@@ -9,6 +9,7 @@ from pprint import pprint
 import yaml
 from tqdm import tqdm
 import requests
+from requests import Session
 
 global vk_session, vk
 
@@ -20,7 +21,7 @@ def auth_handler():
     # Код двухфакторной аутентификации
     key = input("Enter authentication code: ")
     # Если: True - сохранить, False - не сохранять.
-    remember_device = True
+    remember_device = False
 
     return key, remember_device
 
@@ -30,17 +31,12 @@ def captcha_handler(captcha):
         капчи. Через метод get_url можно получить ссылку на изображение.
         Через метод try_again можно попытаться отправить запрос с кодом капчи
     """
-    print("Captcha called")
-    # key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
     image_captcha = ImageCaptcha(rucaptcha_key=rucaptcha_key)
     img_data = requests.get(captcha.get_url()).content
     with open('image_name.jpg', 'wb') as handler:
         handler.write(img_data)
-    # print(captcha.get_url())
-    # key = image_captcha.captcha_handler(captcha_link=captcha.get_url())
-    keyIMG = image_captcha.captcha_handler(captcha_base64=img_data)
-    print(keyIMG)
-    return captcha.try_again(keyIMG["captchaSolve"])
+    key = image_captcha.captcha_handler(captcha_base64=img_data)
+    return captcha.try_again(key["captchaSolve"])
 
 
 # Record system
@@ -55,11 +51,18 @@ recordDaemon = MySqlDaemon(config=cfg)
 
 # vk_session = vk_api.VkApi(phone_number, password, auth_handler=auth_handler, captcha_handler=captcha_handler)
 # vk_session.auth()
-vk_session = vk_api.VkApi(phone_number, password, token=access_token, captcha_handler=captcha_handler)
 
-
-
+vk_session = vk_api.VkApi(phone_number, password, token=access_token, captcha_handler=captcha_handler, api_version='5.92')
+vk_session.http.headers['User-agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0'
 vk = vk_session.get_api()
+
+# user_agent = {"User-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0'}
+# session = Session()
+# session.headers.update(user_agent)
+# session.headers.update({"Authorization": f"Bearer {access_token}"})
+# vk_session = vk_api.VkApi(phone_number, password, session=session, token=access_token, captcha_handler=captcha_handler, api_version='5.92')
+# vk_session.auth(token_only=True)
+# vk = vk_session.get_api()
 
 # GROUP_ID = -188660528
 GROUP_ID = -172053584
@@ -70,11 +73,11 @@ def getData(group_id, voting_flg=True):
     users_and_answers = []
     # TODO: max request by get is 100. Need to use offset
     # 19000
-    offset = 500
+    offset = 0
     while True:
         items = vk.wall.get(owner_id=group_id, count=100, offset=offset)["items"] # 0 -> 100
         # items = vk.wall.get(owner_id=group_id, count=100, offset=100)["items"] # 100 -> 200
-        for item in tqdm(items[80:100], desc="Total Forms"):
+        for item in tqdm(items[:], desc="Total Forms"):
             attachments = item["attachments"]
             # print(attachments)
             for attachment in attachments:
@@ -90,6 +93,7 @@ def getData(group_id, voting_flg=True):
                         insert_query = f"""INSERT INTO FORMS_TABLE (vk_form_id, form_vk_created_date, form_scrapped_date, multiple_answers, form_content) 
                                             VALUES ({poll_id}, {item['date']}, {int(time.time_ns() / 1_000_000)}, "{int(poll['multiple'])}", "{poll['question'].replace('"', "").replace("'", "")}")"""
                         recordDaemon.mysql_post_execution_handler(insert_query)
+                    print(poll)
                     if voting_flg and not poll["closed"] and poll["can_vote"]:
                         print("New VOTE")
                         vk.polls.addVote(
