82af4229387dafd90a3485aa3ef6e95b103ba02d
molozey <lipskii.as@phystech.edu>
2023-02-20 01:15:20 +0300

    pipeline to create components

--- web_cite/src/App.tsx
+++ web_cite/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import DynamicUser from "./components/DynamicUser";
 function App() {
   return (
     <div className="App">
         <h1>Nigger</h1>
+        <h1 color={"red"}>Dynamic User</h1>
+        <h4>EMPTY</h4>
+        <DynamicUser user_id={0}></DynamicUser>
     </div>
   );
 }
